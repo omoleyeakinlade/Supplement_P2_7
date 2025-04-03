b7d1a614855a819f7f331ae62835f7f73b2bfa5b
Omoleye Akinlade <omoleyeakinlade@Omoleyes-MacBook-Pro.local>
2025-04-03 18:01:26 -0400

    test: added test for declaring function to interpret i32 as f32

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,7 @@
-pub fn i32_to_f32(n: i32) -> f32 {
- todo!("Not Implemented")
+#[test]
+fn test_i32_to_f32_and_back() {
+    let i: i32 = 1234567890;
+    let f = i32_to_f32(i);
+    let i2 = f32_to_i32(f);
+    assert_eq!(f32_to_i32(i32_to_f32(i)), i2);
 }
---
+++ target/debug/deps/supplement_p2_7-3bde068b45751bb7.d
@@ -0,0 +1,5 @@
+/Users/omoleyeakinlade/Desktop/Supplement_p2_7/supplement_p2_7/target/debug/deps/supplement_p2_7-3bde068b45751bb7: src/lib.rs
+
+/Users/omoleyeakinlade/Desktop/Supplement_p2_7/supplement_p2_7/target/debug/deps/supplement_p2_7-3bde068b45751bb7.d: src/lib.rs
+
+src/lib.rs:
