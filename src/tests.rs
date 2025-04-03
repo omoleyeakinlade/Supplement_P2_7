d17a6b701917b8dfdd96cea59ab898eec9157aeb
Omoleye Akinlade <omoleyeakinlade@Omoleyes-MacBook-Pro.local>
2025-04-03 18:20:59 -0400

    test: added test for test_u32_to_f32_and_back

--- src/tests.rs
+++ src/tests.rs
@@ -5,3 +5,10 @@ fn test_i32_to_f32_and_back() {
     let i2 = f32_to_i32(f);
     assert_eq!(f32_to_i32(i32_to_f32(i)), i2);
 }
+#[test]
+fn test_u32_to_f32_and_back() {
+    let u: u32 = 0x40400000;
+    let f = u32_to_f32(u);
+    let u2 = f32_to_u32(f);
+    assert_eq!(u, u2);
+}
