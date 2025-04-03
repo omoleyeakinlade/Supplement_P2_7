3b55c4836e24349724db8bafa19fba0201a1258b
Omoleye Akinlade <omoleyeakinlade@Omoleyes-MacBook-Pro.local>
2025-04-03 18:48:07 -0400

    feat: implemented function to convert u32 to a f32

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub fn i32_to_f32(n: i32) -> f32 {
     f32::from_bits(n as u32)
    }
+
    pub fn f32_to_i32(f: f32) -> i32 {
     f.to_bits() as i32
 }
+
 pub fn u32_to_f32(n: u32) -> f32 {
-    todo!("Not Implemented")
+    f32::from_bits(n)
 }
