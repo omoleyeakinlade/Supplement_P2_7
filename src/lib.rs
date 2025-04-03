8a041a25c91107199c7d7ca6d1fb2b1993442585
Omoleye Akinlade <omoleyeakinlade@Omoleyes-MacBook-Pro.local>
2025-04-03 18:06:59 -0400

    feat: implemented function for i32_to_f32

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub fn i32_to_f32(n: i32) -> f32 {
+    f32::from_bits(n as u32)
+   }
