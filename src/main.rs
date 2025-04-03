41e8bfba29c2de54b9629c2c2d1ecd318a0f9310
Omoleye Akinlade <omoleyeakinlade@Omoleyes-MacBook-Pro.local>
2025-04-03 19:32:47 -0400

    feat: add basic example usage in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
- todo!("Not Implemented")
+    println!("Welcome to the Number Conversion App!");
+
+    let int_val: i32 = 1065353216;
+    let float_val = number_converter::i32_to_f32(int_val);
+    println!("i32 {} interpreted as f32: {}", int_val, float_val);
 }
