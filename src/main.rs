95c39f4479ed824b6c0f0c249312156b2fe31ce4
Omoleye Akinlade <omoleyeakinlade@Omoleyes-MacBook-Pro.local>
2025-04-03 22:03:35 -0400

    fix: remove incorrect module name and use proper imported function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     println!("Welcome to the Number Conversion App!");
 
     let int_val: i32 = 1065353216;
-    let float_val = number_converter::i32_to_f32(int_val);
+    let float_val = i32_to_f32(int_val);
     println!("i32 {} interpreted as f32: {}", int_val, float_val);
 }
